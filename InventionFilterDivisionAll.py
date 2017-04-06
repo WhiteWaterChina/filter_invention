2ca1aac8da913b2c87fcde3518de84735beba993
WhiteWaterChina <yanshuo5091@163.com>
2017-04-06 17:03:56 +0800

    20170406
    按照2017财年统计模板修改统计方式，主要是单元格的变更。

--- InventionFilterDivisionAll.py
+++ InventionFilterDivisionAll.py
@@ -14,11 +14,12 @@ import time
 
 filename_original = unicode()
 #listDivisionName=['测试一处'.decode('gbk'), '测试二处'.decode('gbk'), '测试三处'.decode('gbk'), '测试四处'.decode('gbk'), '测试五处'.decode('gbk'), '测试六处'.decode('gbk')]
-listDivisionName=['测试一处'.decode('gbk'), '测试二处'.decode('gbk'), '测试三处'.decode('gbk')]
+listDivisionName=['测试一处'.decode('gbk'), '测试二处'.decode('gbk'), '测试三处'.decode('gbk'), '测试四处'.decode('gbk'), '测试五处'.decode('gbk'), '测试六处'.decode('gbk')]
 listTitle = ['发明提交数量'.decode('gbk'), '发明受理数量'.decode('gbk'), '实用新型提交数量'.decode('gbk'), '实用新型受理数量'.decode('gbk')]
 root = Tkinter.Tk()
 root.title("专利结果过滤工具".decode('gbk'))
-root.geometry('800x600')
+root.geometry('400x300')
+#root.iconbitmap('logo.ico')
 root.resizable(width=True, height=True)
 var_char_entry_filename_need_filter = Tkinter.StringVar()
 
@@ -33,21 +34,23 @@ def get_filename():
 def get_data():
     print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     filename_input = filename_original
-    WorkBook = xlsxwriter.Workbook("测试验证部个人专利完成情况统计.xlsx".decode('gbk'))
+    timestamp = time.strftime('%Y%m%d', time.localtime())
+    WorkBook = xlsxwriter.Workbook("测试验证部个人专利完成情况统计-%s.xlsx".decode('gbk') % timestamp)
     formatOne = WorkBook.add_format()
     formatOne.set_border(1)
     for item in listDivisionName:
         WorkBook.add_worksheet(item)
     for department_to_filter in listDivisionName:
         sheet_now = WorkBook.get_worksheet_by_name(department_to_filter)
+        sheet_now.set_column('B:E', 15)
         for i in range(1, len(listTitle)+1):
             sheet_now.write(0, i, listTitle[i-1], formatOne)
         list_username = []
         file_name = pandas.read_csv(filename_input, sep=',', header=1, encoding='gbk', na_filter=True, chunksize=1)
         for item in file_name:
-            department = item.iloc[0, 0].replace(u' ', u'')
+            department = item.iloc[0, 3].replace(u' ', u'')
             if department == department_to_filter:
-                username = item.iloc[0, 1]
+                username = item.iloc[0, 6]
                 if username not in list_username:
                     list_username.append(username)
         data_display = {}
@@ -59,27 +62,27 @@ def get_data():
             data_display['%s' % name]['实用新型受理数量'.decode('gbk')] = 0
         file_name = pandas.read_csv(filename_input, sep=',', header=1, encoding='gbk', na_filter=True, chunksize=1)
         for item_1 in file_name:
-            department = item_1.iloc[0, 0].replace(u' ', u'')
-            username = item_1.iloc[0, 1].replace(u' ', u'')
-            type_invention = item_1.iloc[0, 6].replace(u' ', u'')
-            date_shouli = item_1.iloc[0, 5]
-            backup = item_1.iloc[0, 8]
-            backup_1 = 0
-            if pandas.notnull(backup):
-                backup_1 = backup[0:2]
+            department = item_1.iloc[0, 3].replace(u' ', u'')
+            username = item_1.iloc[0, 6].replace(u' ', u'')
+            type_invention = item_1.iloc[0, 4].replace(u' ', u'')
+            date_shouli = item_1.iloc[0, 8]
+#            backup = item_1.iloc[0, 8]
+#            backup_1 = 0
+#            if pandas.notnull(backup):
+#                backup_1 = backup[0:2]
 
             if department == department_to_filter:
                 if username in list_username:
                     if pandas.notnull(date_shouli):
                         if type_invention == '发明'.decode('gbk'):
                             data_display['%s' % username]['发明受理数量'.decode('gbk')] += 1
-                        if type_invention == '实用新型'.decode('gbk'):
+                        if type_invention == '新型'.decode('gbk'):
                             data_display['%s' % username]['实用新型受理数量'.decode('gbk')] += 1
-                    if backup_1 not in ['放弃'.decode('gbk'), '15'.decode('gbk')]:
-                        if type_invention == '发明'.decode('gbk'):
-                            data_display['%s' % username]['发明提交数量'.decode('gbk')] += 1
-                        if type_invention == '实用新型'.decode('gbk'):
-                            data_display['%s' % username]['实用新型提交数量'.decode('gbk')] += 1
+#                    if backup_1 not in ['放弃'.decode('gbk'), '15'.decode('gbk')]:
+                    if type_invention == '发明'.decode('gbk'):
+                        data_display['%s' % username]['发明提交数量'.decode('gbk')] += 1
+                    if type_invention == '新型'.decode('gbk'):
+                        data_display['%s' % username]['实用新型提交数量'.decode('gbk')] += 1
 #        print data_display
         for index, item in enumerate(list_username):
             line_count =  index+1
