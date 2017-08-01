6501e3b1c38672d6879b66fb4c024289e0ea2e45
yanshuo <yanshuo5091@163.com>
2017-08-01 17:39:50 +0800

    20170801 修改成按照专利总览和专利受理统计两个文件进行专利统计

--- InventionFilterDivisionAll.py
+++ InventionFilterDivisionAll.py
@@ -1,15 +1,8 @@
 #!/bin/usr/env python
 # -*- coding:cp936 -*-
 
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
 """这个工具的作用是过滤部门的专利总览表，然后统计出g各处个人的专利完成情况。分为四列：发明提交、发明受理、实用新型提交、实用新型受理。"""
 
-import pandas
 import xlsxwriter
 import time
 import os
@@ -17,16 +10,18 @@ import wx
 import wx.xrc
 import xlrd
 
-
-###########################################################################
-## Class InventionFilterAll
-###########################################################################
 DisplayFilename = wx.TextCtrl
 DisplayResultDir = wx.TextCtrl
-filename_original = unicode()
+filename_original_zonglan = unicode()
+filename_original_shouli = unicode()
 filename_allname = unicode()
-listDivisionName=['测试一处'.decode('gbk'), '测试二处'.decode('gbk'), '测试三处'.decode('gbk'), '测试四处'.decode('gbk'), '测试五处'.decode('gbk'), '测试六处'.decode('gbk'), '测试七处'.decode('gbk')]
+dir_filename_display = unicode()
+
+listDivisionName = ['测试一处'.decode('gbk'), '测试二处'.decode('gbk'), '测试三处'.decode('gbk'), '测试四处'.decode('gbk'),
+                    '测试五处'.decode('gbk'), '测试六处'.decode('gbk'), '测试七处'.decode('gbk')]
 listTitle = ['发明提交数量'.decode('gbk'), '发明受理数量'.decode('gbk'), '实用新型提交数量'.decode('gbk'), '实用新型受理数量'.decode('gbk')]
+
+
 # namelist = {}
 # for index_chu, item_chu in enumerate(listDivisionName):
 #     namelist["%s" % item_chu ] = {}
@@ -34,7 +29,7 @@ listTitle = ['发明提交数量'.decode('gbk'), '发明受理数量'.decode('gbk'), '实用新
 
 class InventionFilterAll(wx.Frame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"专利结果过滤工具", pos=wx.DefaultPosition, size=wx.Size(387, 279),
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"专利结果过滤工具", pos=wx.DefaultPosition, size=wx.Size(387, 355),
                           style=wx.CAPTION | wx.RESIZE_BORDER | wx.TAB_TRAVERSAL)
 
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
@@ -45,7 +40,7 @@ class InventionFilterAll(wx.Frame):
 
         bSizer3 = wx.BoxSizer(wx.VERTICAL)
 
-        self.text_1 = wx.StaticText(self, wx.ID_ANY, u"请在如下选择需要处理的专利统计文件", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.text_1 = wx.StaticText(self, wx.ID_ANY, u"请在如下选择需要处理的专利总览文件", wx.DefaultPosition, wx.DefaultSize, 0)
         self.text_1.Wrap(-1)
         self.text_1.SetForegroundColour(wx.Colour(0, 0, 0))
         self.text_1.SetBackgroundColour(wx.Colour(255, 0, 0))
@@ -56,13 +51,36 @@ class InventionFilterAll(wx.Frame):
 
         bSizer2 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.DisplayFilename = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer2.Add(self.DisplayFilename, 1, wx.ALL, 5)
+        self.textctl_zonglan_filename = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                                    0)
+        bSizer2.Add(self.textctl_zonglan_filename, 1, wx.ALL, 5)
 
-        self.ButtonChoseFile = wx.Button(self, wx.ID_ANY, u"选择文件", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.ButtonChoseFile = wx.Button(self, wx.ID_ANY, u"选择专利总览文件", wx.DefaultPosition, wx.DefaultSize, 0)
         bSizer2.Add(self.ButtonChoseFile, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
 
         bSizer1.Add(bSizer2, 0, wx.EXPAND, 5)
+
+        bSizer9 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_staticText4 = wx.StaticText(self, wx.ID_ANY, u"请在如下选择需要处理的专利受理文件", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText4.Wrap(-1)
+        self.m_staticText4.SetForegroundColour(wx.Colour(0, 0, 0))
+        self.m_staticText4.SetBackgroundColour(wx.Colour(255, 0, 0))
+
+        bSizer9.Add(self.m_staticText4, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        bSizer1.Add(bSizer9, 0, wx.EXPAND, 5)
+
+        bSizer10 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.textctl_shouli_filename = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                                   0)
+        bSizer10.Add(self.textctl_shouli_filename, 1, wx.ALL, 5)
+
+        self.m_button6 = wx.Button(self, wx.ID_ANY, u"选择专利受理文件", wx.DefaultPosition, wx.DefaultSize, 0)
+        bSizer10.Add(self.m_button6, 0, wx.ALL, 5)
+
+        bSizer1.Add(bSizer10, 0, wx.EXPAND, 5)
 
         bSizer71 = wx.BoxSizer(wx.VERTICAL)
 
@@ -83,7 +101,7 @@ class InventionFilterAll(wx.Frame):
         self.buttonchosename = wx.Button(self, wx.ID_ANY, u"选择文件", wx.DefaultPosition, wx.DefaultSize, 0)
         bSizer81.Add(self.buttonchosename, 0, wx.ALL, 5)
 
-        bSizer1.Add(bSizer81, 1, wx.EXPAND, 5)
+        bSizer1.Add(bSizer81, 0, wx.EXPAND, 5)
 
         bSizer4 = wx.BoxSizer(wx.VERTICAL)
 
@@ -98,8 +116,8 @@ class InventionFilterAll(wx.Frame):
 
         bSizer7 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.DisplayResultDir = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer7.Add(self.DisplayResultDir, 1, wx.ALL, 5)
+        self.text_dislpay_result = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        bSizer7.Add(self.text_dislpay_result, 1, wx.ALL, 5)
 
         self.ButtonChoseDir = wx.Button(self, wx.ID_ANY, u"选择目录", wx.DefaultPosition, wx.DefaultSize, 0)
         bSizer7.Add(self.ButtonChoseDir, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
@@ -122,7 +140,8 @@ class InventionFilterAll(wx.Frame):
         self.Centre(wx.BOTH)
 
         # Connect Events
-        self.ButtonChoseFile.Bind(wx.EVT_BUTTON, self.get_filename)
+        self.ButtonChoseFile.Bind(wx.EVT_BUTTON, self.get_zonglan_filename)
+        self.m_button6.Bind(wx.EVT_BUTTON, self.get_shouli_filename)
         self.buttonchosename.Bind(wx.EVT_BUTTON, self.get_allname)
         self.ButtonChoseDir.Bind(wx.EVT_BUTTON, self.set_filename)
         self.button_go.Bind(wx.EVT_BUTTON, self.get_data)
@@ -135,18 +154,30 @@ class InventionFilterAll(wx.Frame):
         self.Close()
 
     # Virtual event handlers, overide them in your derived class
-    def get_filename(self, event):
-        global filename_original
-        filename_invention_dialog = wx.FileDialog(self, message="选择专利文件".decode('gbk'), defaultDir=os.getcwd(), defaultFile="")
+    def get_zonglan_filename(self, event):
+        global filename_original_zonglan
+        filename_invention_dialog = wx.FileDialog(self, message="选择专利总览文件".decode('gbk'), defaultDir=os.getcwd(),
+                                                  defaultFile="")
         if filename_invention_dialog.ShowModal() == wx.ID_OK:
             filename_invention = filename_invention_dialog.GetPath()
-            self.DisplayFilename.SetValue(filename_invention)
-            filename_original = filename_invention
+            self.textctl_zonglan_filename.SetValue(filename_invention)
+            filename_original_zonglan = filename_invention
+            filename_invention_dialog.Destroy()
+
+    def get_shouli_filename(self, event):
+        global filename_original_shouli
+        filename_invention_dialog = wx.FileDialog(self, message="选择专利受理文件".decode('gbk'), defaultDir=os.getcwd(),
+                                                  defaultFile="")
+        if filename_invention_dialog.ShowModal() == wx.ID_OK:
+            filename_invention = filename_invention_dialog.GetPath()
+            self.textctl_shouli_filename.SetValue(filename_invention)
+            filename_original_shouli = filename_invention
             filename_invention_dialog.Destroy()
 
     def get_allname(self, event):
         global filename_allname
-        filename_invention_dialog = wx.FileDialog(self, message="选择人员名单文件".decode('gbk'), defaultDir=os.getcwd(), defaultFile="")
+        filename_invention_dialog = wx.FileDialog(self, message="选择人员名单文件".decode('gbk'), defaultDir=os.getcwd(),
+                                                  defaultFile="")
         if filename_invention_dialog.ShowModal() == wx.ID_OK:
             all_name = filename_invention_dialog.GetPath()
             self.textctrl_allname.SetValue(all_name)
@@ -158,17 +189,16 @@ class InventionFilterAll(wx.Frame):
         dir_filename_display_dialog = wx.DirDialog(self, message="选择存储目录".decode('gbk'), style=wx.DD_DEFAULT_STYLE)
         if dir_filename_display_dialog.ShowModal() == wx.ID_OK:
             dir_filename_display = dir_filename_display_dialog.GetPath()
-#            .replace('/', '\\')
-            self.DisplayResultDir.SetValue(dir_filename_display)
+            #            .replace('/', '\\')
+            self.text_dislpay_result.SetValue(dir_filename_display)
             dir_filename_display_dialog.Destroy()
-
 
     def get_data(self, event):
         print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         filename_name = xlrd.open_workbook(filename_allname, encoding_override='cp936')
         namelist = {}
         data_display = {}
-#处理人员名称
+        # 处理人员名称
         for item_chu in listDivisionName:
             namelist["%s" % item_chu] = []
         for item_chu in listDivisionName:
@@ -186,24 +216,25 @@ class InventionFilterAll(wx.Frame):
                 data_display["%s" % item_chu]["%s" % name_people]['实用新型提交数量'.decode('gbk')] = 0
                 data_display["%s" % item_chu]["%s" % name_people]['实用新型受理数量'.decode('gbk')] = 0
 
-        # 处理统计数据
-        filename_input = filename_original
-        file_name = xlrd.open_workbook(filename_input, encoding_override='cp936')
+        # 处理总览统计数据，查询并统计撰写通过的专利
+        # filename_input = filename_original_zonglan
+        file_name = xlrd.open_workbook(filename_original_zonglan, encoding_override='cp936')
         sheet_filter_one = file_name.sheet_by_index(0)
         total_rows_one = sheet_filter_one.nrows
         sheet_filter_two = file_name.sheet_by_index(1)
         total_rows_two = sheet_filter_two.nrows
 
         list_status = ["撰写通过".decode('gbk')]
-        username = ''
+        # username = ''
         for item_1 in range(1, total_rows_one):
             username = sheet_filter_one.cell(item_1, 6).value
-            type_invention = sheet_filter_one.cell(item_1, 4).value.replace(u' ', u'').split(",")[0]
-            shouli_or_not = sheet_filter_one.cell(item_1, 8).value
+            type_invention = sheet_filter_one.cell(item_1, 4).value.replace(u' ', u'').split(",")[0].strip()
+            # shouli_or_not = sheet_filter_one.cell(item_1, 8).value
             status = sheet_filter_one.cell(item_1, 0).value.strip()
             for item_chu in listDivisionName:
                 if username in namelist["%s" % item_chu]:
-                    if shouli_or_not != 'None' or status in list_status:
+                    # if shouli_or_not != 'None' or status in list_status:
+                    if status in list_status:
                         if type_invention == '发明'.decode('gbk'):
                             data_display["%s" % item_chu]['%s' % username]['发明受理数量'.decode('gbk')] += 1
                         if type_invention == '新型'.decode('gbk'):
@@ -215,7 +246,7 @@ class InventionFilterAll(wx.Frame):
 
         for item_2 in range(1, total_rows_two):
             username = sheet_filter_two.cell(item_2, 1).value
-            type_invention = sheet_filter_two.cell(item_2, 6).value.replace(u' ', u'')
+            type_invention = sheet_filter_two.cell(item_2, 6).value.replace(u' ', u'').strip()
             for item_chu in listDivisionName:
                 if username in namelist["%s" % item_chu]:
                     if type_invention == '发明'.decode('gbk'):
@@ -223,7 +254,21 @@ class InventionFilterAll(wx.Frame):
                     if type_invention == '实用新型'.decode('gbk'):
                         data_display["%s" % item_chu]['%s' % username]['实用新型受理数量'.decode('gbk')] += 1
 
-#write output excel
+        # 处理总览统计数据，查询并统计撰写通过的专利
+        file_name_shouli = xlrd.open_workbook(filename_original_shouli, encoding_override='cp936')
+        sheet_filter_shouli = file_name_shouli.sheet_by_index(0)
+        total_rows_shouli = sheet_filter_shouli.nrows
+        for item_shouli in range(1, total_rows_shouli):
+            username_shouli = sheet_filter_shouli.cell(item_shouli, 4).value.strip()
+            type_invention = sheet_filter_shouli.cell(item_shouli, 1).value.split(",")[0].strip()
+            for item_chu in listDivisionName:
+                if username_shouli in namelist["%s" % item_chu]:
+                    if type_invention == '发明'.decode('gbk'):
+                        data_display["%s" % item_chu]['%s' % username_shouli]['发明受理数量'.decode('gbk')] += 1
+                    if type_invention == '新型'.decode('gbk'):
+                        data_display["%s" % item_chu]['%s' % username_shouli]['实用新型受理数量'.decode('gbk')] += 1
+
+        # write output excel
         timestamp = time.strftime('%Y%m%d', time.localtime())
         filename_display = "测试验证部个人专利完成情况统计-%s.xlsx".decode('gbk') % timestamp
         filename_final = os.path.join(dir_filename_display, filename_display)
@@ -241,13 +286,20 @@ class InventionFilterAll(wx.Frame):
             for index, item_sheet_one in enumerate(namelist["%s" % item_chu]):
                 line_count = index + 1
                 sheet_now.write(line_count, 0, item_sheet_one, formatone)
-                sheet_now.write(line_count, 1, data_display["%s" % item_chu]['%s' % item_sheet_one]['发明提交数量'.decode('gbk')], formatone)
-                sheet_now.write(line_count, 2, data_display["%s" % item_chu]['%s' % item_sheet_one]['发明受理数量'.decode('gbk')], formatone)
-                sheet_now.write(line_count, 3, data_display["%s" % item_chu]['%s' % item_sheet_one]['实用新型提交数量'.decode('gbk')], formatone)
-                sheet_now.write(line_count, 4, data_display["%s" % item_chu]['%s' % item_sheet_one]['实用新型受理数量'.decode('gbk')], formatone)
+                sheet_now.write(line_count, 1,
+                                data_display["%s" % item_chu]['%s' % item_sheet_one]['发明提交数量'.decode('gbk')], formatone)
+                sheet_now.write(line_count, 2,
+                                data_display["%s" % item_chu]['%s' % item_sheet_one]['发明受理数量'.decode('gbk')], formatone)
+                sheet_now.write(line_count, 3,
+                                data_display["%s" % item_chu]['%s' % item_sheet_one]['实用新型提交数量'.decode('gbk')],
+                                formatone)
+                sheet_now.write(line_count, 4,
+                                data_display["%s" % item_chu]['%s' % item_sheet_one]['实用新型受理数量'.decode('gbk')],
+                                formatone)
 
         print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        diag_finish = wx.MessageDialog(None, '处理结果已经保存到文件%s《%s》中.如果无需其他动作，请点击退出按钮退出程序'.decode('gbk')  % (dir_filename_display, filename_display), '提示'.decode('gbk'), wx.OK |wx.ICON_INFORMATION | wx.STAY_ON_TOP  )
+        diag_finish = wx.MessageDialog(None, '处理结果已经保存到文件%s《%s》中.如果无需其他动作，请点击退出按钮退出程序'.decode('gbk') % (
+            dir_filename_display, filename_display), '提示'.decode('gbk'), wx.OK | wx.ICON_INFORMATION | wx.STAY_ON_TOP)
         diag_finish.ShowModal()
         workbook_to_write.close()
 
